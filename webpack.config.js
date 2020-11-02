c154e0561dbaee84708c361f0e2017cf523f6ebf
Futahei-HomeDesktop <futahei1108@icloud.com>
2020-11-02 21:59:08 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: process.env.NODE_ENV || "development",
+  entry: {
+    main: path.join(__dirname, "src/main.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "dist/js"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new CopyPlugin([{ from: ".", to: "../"}], { context: "public" })
+  ],
+};
