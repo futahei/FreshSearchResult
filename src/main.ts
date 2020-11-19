8747b07708b851dee4b1deee26b1c971bd4cd429
Futahei-HomeDesktop <futahei1108@icloud.com>
2020-11-19 17:38:16 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ const BASE = NOW.getTime() - OLDEST.getTime();
 const ONEMINUSCOLOR = [255-205, 255-170, 255-85];
 
 $(function() {
-  // $("body").css("background-color", "#CCC6B8");
   $(".g").each(function(index: number, element: HTMLElement) {
     const span = $(element).find("span.f");
     console.log(index, span.length);
@@ -17,7 +16,6 @@ $(function() {
         const date = new Date(dataString);
         const elapsed = NOW.getTime() - date.getTime();
         const density = 1 - Math.max(Math.min(elapsed / BASE, 1), 0);
-        // element.style.backgroundColor = `rgba(${205+ONEMINUSCOLOR[0]*density}, ${170+ONEMINUSCOLOR[1]*density}, ${85+ONEMINUSCOLOR[2]*density}, 0.5)`;
         element.style.opacity = (Math.max(density, 0.1)).toString();
         return;
       }
