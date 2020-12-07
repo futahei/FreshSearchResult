9d3a05b957dce93fa8d0242356394cfe08280517
Futahei-HomeDesktop <futahei1108@icloud.com>
2020-12-07 15:41:47 +0900

    add scrollbar for control base year

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import $ from 'jquery';
 
-const NOW = new Date();
-const OLDEST = new Date(NOW.getFullYear() - 5, NOW.getMonth(), NOW.getDate(), NOW.getHours(), NOW.getMinutes(), NOW.getSeconds(), NOW.getMilliseconds());
-const BASE = NOW.getTime() - OLDEST.getTime();
-const ONEMINUSCOLOR = [255-205, 255-170, 255-85];
+let range: JQuery<HTMLElement>;
 
-$(function() {
+const main = function() {
+  const baseYear = Number(range.val());
+  $("#baseYear").text(baseYear);
+  const NOW = new Date();
+  const OLDEST = new Date(NOW.getFullYear() - Number(range.val()), NOW.getMonth(), NOW.getDate(), NOW.getHours(), NOW.getMinutes(), NOW.getSeconds(), NOW.getMilliseconds());
+  const BASE = NOW.getTime() - OLDEST.getTime();
+
   $(".g").each(function(index: number, element: HTMLElement) {
     const span = $(element).find("span.f");
-    console.log(index, span.length);
     if (span.length > 0) {
       const dataString = span[0].innerHTML.substring(0, 10);
       if (dataString.match(/\d{4}\/\d{2}\/\d{2}/))
@@ -22,4 +24,18 @@ $(function() {
     }
     element.style.backgroundColor = `rgba(51, 221, 255, 0.2)`;
   });
+}
+
+$(function() {
+  $("<span>基準年数：<span id='baseYear'>0</span>年</span>").appendTo("#result-stats");
+  range = $("<input>").attr({
+    type: "range",
+    id: "elapsedBaseYear",
+    min: "1",
+    max: "10",
+    value: "5"
+  }).appendTo("#result-stats");
+  range.on("input", main);
+
+  main();
 });
