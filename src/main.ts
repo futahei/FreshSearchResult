053cc30baeee0c74f913559393163d0cd3c02808
Futahei-HomeDesktop <futahei1108@icloud.com>
2020-12-28 00:38:14 +0900

    del unknown one's effect

--- src/main.ts
+++ src/main.ts
@@ -11,17 +11,19 @@ const update = function(element: HTMLElement, density: number, mode: number) {
     "background-color": `rgba(255,255,255,${__density})`,
     "background-blend-mode": "lighten"
   });
-  $(element).find("span, cite, em").filter((i: number, e: HTMLSpanElement) => {
-    return e.innerText.length > 0;
-  }).each((i: number, e: HTMLSpanElement) => {
-    const d = Math.round(((1-__density)*4)*1000)/1000;
-    let rgb = $(e).css("color").replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+)\)/i);
-    if (!rgb) rgb = $(e).css("text-shadow").match(/^rgba?\((\d+), (\d+), (\d+)/i);
-    $(e).css({
-      "color": "transparent",
-      "text-shadow": `0 0 ${d}px rgba(${rgb?rgb[1]:0}, ${rgb?rgb[2]:0}, ${rgb?rgb[3]:0}, ${d>0?0.5:1.0})`
+  if (density >= 0) {
+    $(element).find("span, cite, em").filter((i: number, e: HTMLSpanElement) => {
+      return e.innerText.length > 0;
+    }).each((i: number, e: HTMLSpanElement) => {
+      const d = Math.round(((1-__density)*4)*1000)/1000;
+      let rgb = $(e).css("color").replace(/\s/g, '').match(/^rgba?\((\d+),(\d+),(\d+)\)/i);
+      if (!rgb) rgb = $(e).css("text-shadow").match(/^rgba?\((\d+), (\d+), (\d+)/i);
+      $(e).css({
+        "color": "transparent",
+        "text-shadow": `0 0 ${d}px rgba(${rgb?rgb[1]:0}, ${rgb?rgb[2]:0}, ${rgb?rgb[3]:0}, ${d>0?0.5:1.0})`
+      });
     });
-  });
+  }
 }
 
 const main = function(year: number, mode: number) {
@@ -33,7 +35,7 @@ const main = function(year: number, mode: number) {
 
   $(".g").each(function(index: number, element: HTMLElement) {
     const span = $(element).find("span.f");
-    let density = 0.2;
+    let density = -1.0;
     if (span.length > 0) {
       let date: Date|null = null;
       if (span[0].innerHTML.substring(0, 10).match(/\d{4}\/\d{2}\/\d{2}/)) {
@@ -73,28 +75,6 @@ $(function() {
       main(Number(range.val()), Number($("#result-stats").children('input[name="drawMode"]:checked').attr("value")));
       chrome.storage.sync.set({"fsr-year": Number(range.val())});
     });
-
-    /*
-    for (let i = 0; i < 3; i++) {
-      const radio = $("<input>").attr({
-        type: "radio",
-        name: "drawMode",
-        value: i,
-        checked: res["fsr-mode"]==i
-      }).appendTo("#result-stats");
-      let mode: string = "";
-      switch(i) {
-        case 0: mode = "noise"; break;
-        case 1: mode = "grayscale"; break;
-        case 2: mode = "oldness"; break;
-      }
-      $(`<span>${mode}</span>`).appendTo("#result-stats");
-      radio.on("change", () => {
-        main(Number(range.val()), Number(radio.val()));
-        chrome.storage.sync.set({"fsr-mode": Number(radio.val())});
-      });
-    }
-    */
 
     init(res["fsr-mode"]);
 
